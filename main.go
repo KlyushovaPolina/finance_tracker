c2cd63963dc817f604a706b01705cf5bd4194856
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-06-29 19:21:15 +0300

    restructure code organization

--- main.go
+++ main.go
@@ -1,52 +1,59 @@
 package main
 
-import(
-    "gorm.io/gorm" //для работы с бд (ORM)
-	"gorm.io/driver/postgres" //драйвер для постгри
-	"time"
+import (
 	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres" //драйвер для постгри
+	"gorm.io/gorm"            //для работы с бд (ORM)
 )
 
-func main(){
-	type Transaction struct{ //модель
-		ID uint
-		Amount float64
-		Type string //тип транзакции - трата или расход
-		Category string //категория - еда, одежда и тд
-		Description *string //может быть пустым
-		Date time.Time
-		CreatedAt time.Time //автоматически создается GORM
+type Transaction struct { //модель
+	ID          uint
+	Amount      float64
+	Type        string  //тип транзакции - трата или расход
+	Category    string  //категория - еда, одежда и тд
+	Description *string //может быть пустым
+	Date        time.Time
+	CreatedAt   time.Time //автоматически создается GORM
+}
+
+var db *gorm.DB
+
+
+func GetTransaction(c *fiber.Ctx) error { //обрабатываем HTTP-метод GET.
+		// c *fiber.Ctx - указатель на контекст запроса
+		// error - тип, возвращаемый функцией
+		// определяем функцию, которая вызывается когда поступает запрос
+		var transactions []Transaction //создаем срез для хранения списка транзакций из бд
+		db.Find(&transactions)
+		return c.JSON(transactions)
 	}
 
+
+func PostTransactions(c *fiber.Ctx) error {
+		transaction := new(Transaction) //возвращаем указатель на пустую структуру
+		c.BodyParser(transaction)       //записывает данные из запроса в структуру
+		db.Create(transaction)
+		return c.Status(201).JSON(transaction)
+	}
+
+func main() {
 	dsn := "host=localhost user=postgres password=postgres dbname=finance-tracker port=5432 sslmode=disable TimeZone=Europe/Moscow" //data source name
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-    	log.Fatal("Ошибка подключения к базе данных:", err) //выводит сообщение и завершает программу
+		log.Fatal("Ошибка подключения к базе данных:", err) //выводит сообщение и завершает программу
 	}
 
 	db.AutoMigrate(&Transaction{}) //передаем указатель на созданный пустой экземпляр структуры
 
-
 	app := fiber.New() //экземпляр fiber
 
-	app.Get("/transactions", func(c *fiber.Ctx) error { //обрабатываем HTTP-метод GET.
-		// c *fiber.Ctx - указатель на контекст запроса
-		// error - тип, возвращаемый функцией
-		// определяем функцию, которая вызывается когда поступает запрос
-		
-		var transactions []Transaction //создаем срез для хранения списка транзакций из бд
-		db.Find(&transactions)
-		return c.JSON(transactions)
-	})
-
-	app.Post("/transactions", func(c *fiber.Ctx) error {
-		transaction := new(Transaction) //возвращаем указатель на пустую структуру
-		c.BodyParser(transaction) //записывает данные из запроса в структуру
-		db.Create(transaction)
-		return c.Status(201).JSON(transaction)
-	})
+	app.Get("/transactions", GetTransaction)
+	app.Post("/transactions", PostTransactions)
 
 	app.Listen(":3000")
 }
