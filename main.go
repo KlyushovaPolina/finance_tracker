44f30105940d769b28970b909fbf61ce00c2c9a8
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-06-30 20:05:21 +0300

    add functions to generate abd verify token

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	_ "finance-tracker/docs"
 
 	"golang.org/x/crypto/bcrypt" //для хеширования пароля
+	"github.com/golang-jwt/jwt/v5" //для генерации токена
 )
 
 type Transaction struct { //модель
@@ -33,14 +34,45 @@ type User struct {
 	PasswordHash string `gorm:"not null"`
 }
 
+//хэширование пароля
 func GeneratePassword(p string) string { //возвращает хеш пароля
 	hash, _ := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
 	return string(hash)
 }
-
 func ComparePassword(hashedPassword, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+//генерация токена
+func GenerateToken(id uint) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{ //создаем токен
+		"user_id": id,
+	})
+
+	secret_key := os.Getenv("JWT_SECRET")
+
+	t, err := token.SignedString(secret_key) //подписываем токен секретным ключом
+		if err != nil {
+			return "", err
+	}
+
+	return t, nil
+	}
+
+func VerifyToken(tokenString string) (bool, error) {
+	secret_key := os.Getenv("JWT_SECRET")
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret_key), nil
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+ 	return token.Valid, nil
 }
 
 var db *gorm.DB
